4b518484faecc765e19c9712cb5d47f4d5cc9b5d
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-11 19:17:36 +0200

    The journey begins

---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Tests/Posting.feature
@@ -0,0 +1,9 @@
+﻿Feature: Posting
+	In order to record my thoughts
+	As a user
+	I want to post to my wall
+
+Scenario: Post a message
+	Given I'm using the system as "Alice"
+	When I post the message "I love the weather today"
+	Then I should see "Alice -> I love weather today"
---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Tests/PostingSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Kata.SocialNetworking.Tests
+{
+    [Binding]
+    public class PostingSteps
+    {
+        [Given(@"I'm using the system as ""(.*)""")]
+        public void GivenIMUsingTheSystemAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I post the message ""(.*)""")]
+        public void WhenIPostTheMessage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see ""(.*)""")]
+        public void ThenIShouldSee(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
