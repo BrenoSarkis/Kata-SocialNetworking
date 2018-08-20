57a2b91cdbeef1bc4dab487ab023d5f2b0510564
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-20 18:51:50 +0200

    First infrastructure test to support sending commands. Design looks meh, improvements to come as new scenarios emerge

---
+++ Kata.SocialNetworking/Kata.SocialNetwork.Infrastructure.IntegrationTests/WhenSendingCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Commands;
+
+namespace Kata.SocialNetwork.Infrastructure.IntegrationTests
+{
+    [TestFixture]
+    public class WhenSendingCommand
+    {
+        [Test]
+        public void ItIsDeliveredToItsHandler()
+        {
+            var dummyCommand = new DummyCommand();
+            var bus = new Bus();
+            var commandHandlerMock = Substitute.For<IHandleCommandsOf<DummyCommand>>();
+
+            bus.RegisterCommandHandler(commandHandlerMock);
+
+            bus.SendCommand(dummyCommand);
+
+            commandHandlerMock.Received().Handle(Arg.Any<DummyCommand>());
+        }
+    }
+
+    public interface IHandleCommandsOf<TCommandType> where TCommandType : Command
+    {
+        void Handle(TCommandType command);
+    }
+
+    public class DummyCommand : Command
+    {
+    }
+
+    public class Command
+    {
+    }
+
+    public class Bus
+    {
+        private readonly Dictionary<Type, Action<Command>> commandHandlers = new Dictionary<Type, Action<Command>>();
+
+        public void SendCommand<TCommandType>(TCommandType command) where TCommandType : Command
+        {
+            Action<Command> handler;
+
+            if (commandHandlers.TryGetValue(typeof(TCommandType), out handler))
+            {
+                handler.Invoke(command);
+            }
+        }
+
+        public void RegisterCommandHandler<TCommandType>(IHandleCommandsOf<TCommandType> commandHandler) where TCommandType : Command
+        {
+            commandHandlers.Add(typeof(TCommandType), command => commandHandler.Handle((TCommandType)command));
+        }
+    }
+}
