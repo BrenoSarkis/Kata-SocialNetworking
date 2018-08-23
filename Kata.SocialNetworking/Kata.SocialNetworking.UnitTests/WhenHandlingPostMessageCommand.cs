e94eab9a13083eff31e5042abdf15c49eae06b59
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-23 19:50:33 +0200

    WhenHandlingPostMessageCommand.RaisesMessagePostedEvent

--- Kata.SocialNetworking/Kata.SocialNetworking.UnitTests/WhenHandlingPostMessageCommand.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.UnitTests/WhenHandlingPostMessageCommand.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Kata.SocialNetworking.Infrastructure;
+using Kata.SocialNetworking.Infrastructure.Messaging;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Kata.SocialNetworking.UnitTests
 {
@@ -6,8 +9,57 @@ namespace Kata.SocialNetworking.UnitTests
     public class WhenHandlingPostMessageCommand
     {
         [Test]
-        public void T()
+        public void RaisesMessagePostedEvent()
         {
+            var messagePostedHandler = Substitute.For<IHandleMessagesOf<MessagePosted>>();
+            var command = new PostMessage("aUserName", "aMessage");
+            var bus = new Bus();
+
+            bus.RegisterHandlers(new PostMessageHandler(bus));
+            bus.RegisterHandlers(messagePostedHandler);
+
+            bus.SendCommand(command);
+
+            messagePostedHandler.Received().Handle(Arg.Is<MessagePosted>(mp => mp.UserName == "aUserName" && mp.Message == "aMessage"));
+        }
+    }
+
+    public class PostMessage : ICommand
+    {
+        public string UserName { get; }
+        public string Message { get; }
+
+        public PostMessage(string userName, string message)
+        {
+            UserName = userName;
+            Message = message;
+        }
+    }
+
+    public class PostMessageHandler : IHandleMessagesOf<PostMessage>
+    {
+        private readonly Bus bus;
+
+        public PostMessageHandler(Bus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Handle(PostMessage message)
+        {
+            this.bus.Publish(new MessagePosted(message.UserName, message.Message));
+        }
+    }
+
+    public class MessagePosted : IEvent
+    {
+        public string UserName { get; }
+        public string Message { get; }
+
+        public MessagePosted(string userName, string message)
+        {
+            UserName = userName;
+            Message = message;
         }
     }
 }
