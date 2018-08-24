80acae33290bf5137b643324c7597608de1525b4
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-24 15:06:52 +0200

    Renames infrastructure unit test project to the correct naming

---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenPublishingEvents.cs
@@ -0,0 +1,37 @@
+﻿using Kata.SocialNetworking.Infrastructure.Messaging;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Kata.SocialNetworking.Infrastructure.UnitTests
+{
+    [TestFixture]
+    public class WhenPublishingEvents
+    {
+        [Test]
+        public void DeliverThemToSubscribers()
+        {
+            var id = 1;
+            var dummyEvent = new DummyEvent(id: id);
+            var bus = new Bus();
+            var aHandler = Substitute.For<IHandleMessagesOf<DummyEvent>>();
+            var anotherHandler = Substitute.For<IHandleMessagesOf<DummyEvent>>();
+
+            bus.RegisterHandlers(aHandler, anotherHandler);
+
+            bus.Publish(dummyEvent);
+
+            aHandler.Received().Handle(Arg.Is<DummyEvent>(@event => @event.Id == id));
+            anotherHandler.Received().Handle(Arg.Is<DummyEvent>(@event => @event.Id == id));
+        }
+    }
+
+    public class DummyEvent : IEvent
+    {
+        public int Id { get; }
+
+        public DummyEvent(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenSendingCommand.cs
@@ -0,0 +1,63 @@
+﻿using Kata.SocialNetworking.Infrastructure.Exceptions;
+using Kata.SocialNetworking.Infrastructure.Messaging;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Kata.SocialNetworking.Infrastructure.UnitTests
+{
+    [TestFixture]
+    public class WhenSendingCommand
+    {
+        private Bus bus;
+        private DummyCommand dummyCommand;
+        private int id = 1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            bus = new Bus();
+            dummyCommand = new DummyCommand(id);
+        }
+
+        [Test]
+        public void ItIsDeliveredToItsHandler()
+        {
+            var commandHandlerMock = Substitute.For<IHandleMessagesOf<DummyCommand>>();
+
+            bus.RegisterHandlers(commandHandlerMock);
+
+            bus.SendCommand(dummyCommand);
+
+            commandHandlerMock.Received().Handle(Arg.Is<DummyCommand>(command => command.Id == id));
+        }
+
+        [Test]
+        public void ThrowsWhenHandlerIsNotRegistered()
+        {
+            Assert.That(() => bus.SendCommand(dummyCommand), 
+                Throws.TypeOf<HandlerNotFoundException>().With.Message.EqualTo("No handler found for command DummyCommand"));
+        }
+
+        [Test]
+        public void ThrowsWhenThereAreMoreThanOneHandler()
+        {
+            var aHandler = Substitute.For<IHandleMessagesOf<DummyCommand>>();
+            var anotherHandler = Substitute.For<IHandleMessagesOf<DummyCommand>>();
+
+            bus.RegisterHandlers(aHandler);
+
+            Assert.That(() => bus.RegisterHandlers(anotherHandler),
+                Throws.TypeOf<HandlerAlreadyRegisteredException>().With.Message.EqualTo("There is handler already registered for DummyCommand"));
+        }
+    }
+
+    public class DummyCommand : ICommand
+    {
+        public int Id { get; }
+
+        public DummyCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
