f276bfdc429d86bf13f5d056038dccb2f3b7a0b2
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-26 20:21:35 +0200

    Just skeleton of WhenSavingEvents.EventsAreStored

---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenSavingEvents.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Kata.SocialNetworking.Infrastructure.UnitTests
+{
+    [TestFixture]
+    public class WhenSavingEvents
+    {
+        [Test]
+        public void EventsAreStored()
+        {
+            var eventStore = new EventStore();
+
+            eventStore.Save(new DummyEvent(1));
+        }
+    }
+
+    public class EventStore
+    {
+        public void Save(DummyEvent dummyEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
