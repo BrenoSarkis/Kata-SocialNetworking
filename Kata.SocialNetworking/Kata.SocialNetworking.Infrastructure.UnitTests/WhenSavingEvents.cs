e6bbf3f9b33ad7ab2c80219e13db251dc6d7b4de
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-27 19:17:37 +0200

    Moves event store away from tests. It does not belong to infrastructure either, correct project tbc

--- Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenSavingEvents.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenSavingEvents.cs
@@ -21,20 +21,4 @@ namespace Kata.SocialNetworking.Infrastructure.UnitTests
             Assert.That(newlyStoredEvent.Id, Is.EqualTo(1));
         }
     }
-
-    public class EventStore
-    {
-        private readonly Dictionary<Guid, List<IEvent>> events = new Dictionary<Guid, List<IEvent>>();
-
-        public void Save(Guid aggregateId, IEvent @event)
-        {
-            if (events.ContainsKey(aggregateId)) events[aggregateId].Add(@event);
-            else events[aggregateId] = new List<IEvent> { @event };
-        }
-
-        public IEvent[] GetEventsForAggregate(Guid aggregateId)
-        {
-            return events[aggregateId].ToArray();
-        }
-    }
 }
---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure/EventStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Kata.SocialNetworking.Infrastructure.Messaging;
+
+namespace Kata.SocialNetworking.Infrastructure
+{
+    public class EventStore
+    {
+        private readonly Dictionary<Guid, List<IEvent>> events = new Dictionary<Guid, List<IEvent>>();
+
+        public void Save(Guid aggregateId, IEvent @event)
+        {
+            if (events.ContainsKey(aggregateId)) events[aggregateId].Add(@event);
+            else events[aggregateId] = new List<IEvent> { @event };
+        }
+
+        public IEvent[] GetEventsForAggregate(Guid aggregateId)
+        {
+            return events[aggregateId].ToArray();
+        }
+    }
+}
