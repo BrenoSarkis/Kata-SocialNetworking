54281d88e09d5eeaa8fcdf74bfca3fcac7bbcb3c
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-27 19:15:39 +0200

    Closes up a poor man's version of eventStore, design needs further adjusments

--- Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenSavingEvents.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Infrastructure.UnitTests/WhenSavingEvents.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kata.SocialNetworking.Infrastructure.Messaging;
+using NUnit.Framework;
 
 namespace Kata.SocialNetworking.Infrastructure.UnitTests
 {
@@ -8,17 +12,30 @@ namespace Kata.SocialNetworking.Infrastructure.UnitTests
         [Test]
         public void EventsAreStored()
         {
+            var aggregateId = Guid.Parse("CE1B3BFC-D7BA-4734-826A-7D5C59DB43DE");
             var eventStore = new EventStore();
 
-            eventStore.Save(new DummyEvent(1));
+            eventStore.Save(aggregateId, new DummyEvent(1));
+            var newlyStoredEvent = eventStore.GetEventsForAggregate(aggregateId);
+
+            Assert.That(newlyStoredEvent.Count(), Is.EqualTo(1));
+            Assert.That(newlyStoredEvent.Cast<DummyEvent>().Single().Id, Is.EqualTo(1));
         }
     }
 
     public class EventStore
     {
-        public void Save(DummyEvent dummyEvent)
+        private readonly Dictionary<Guid, List<IEvent>> events = new Dictionary<Guid, List<IEvent>>();
+
+        public void Save(Guid aggregateId, IEvent @event)
         {
-            throw new System.NotImplementedException();
+            if (events.ContainsKey(aggregateId)) events[aggregateId].Add(@event);
+            else events[aggregateId] = new List<IEvent> { @event };
+        }
+
+        public IEvent[] GetEventsForAggregate(Guid aggregateId)
+        {
+            return events[aggregateId].ToArray();
         }
     }
 }
