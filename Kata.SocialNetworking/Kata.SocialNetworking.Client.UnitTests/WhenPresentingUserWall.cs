e0c7e481d678858d8e0bd87d5aef19d2f48d50f5
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-29 18:22:40 +0200

    Clean up

--- Kata.SocialNetworking/Kata.SocialNetworking.Client.UnitTests/WhenPresentingUserWall.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Client.UnitTests/WhenPresentingUserWall.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using Kata.SocialNetworking.Messages.Post;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 namespace Kata.SocialNetworking.Client.UnitTests
 {
