e2c5a5d5c7500e72efbe80a3038ae26d83c72022
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-29 18:35:09 +0200

    References clean up

--- Kata.SocialNetworking/Kata.SocialNetworking.Client.UnitTests/WhenPresentingUserWall.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Client.UnitTests/WhenPresentingUserWall.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Kata.SocialNetworking.Infrastructure.Clock;
+﻿using Kata.SocialNetworking.Infrastructure.Clock;
 using Kata.SocialNetworking.Messages.Post;
 using NUnit.Framework;
 
