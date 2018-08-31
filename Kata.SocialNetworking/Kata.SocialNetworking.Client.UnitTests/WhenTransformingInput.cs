427d4bf89e49fc1f386b367e31586965ca81ccb1
Breno Sarkis <sarkisbreno@gmail.com>
2018-08-31 17:07:19 +0200

    WhenTransformingInput.SuccessfullyParsesASendCommand

--- Kata.SocialNetworking/Kata.SocialNetworking.Client.UnitTests/WhenTransformingInput.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Client.UnitTests/WhenTransformingInput.cs
@@ -1,13 +1,34 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Kata.SocialNetworking.Infrastructure.Messaging;
+using Kata.SocialNetworking.Messages.Post;
+using NUnit.Framework;
 
 namespace Kata.SocialNetworking.Client.UnitTests
 {
     [TestFixture]
     public class WhenTransformingInput
     {
-        public void T()
+        [Test]
+        public void SuccessfullyParsesASendCommand()
         {
+            var inputParser = new InputParser();
 
+            var command = inputParser.Parse("Alice -> hello!");
+
+            Assert.That(command, Is.TypeOf<PostMessage>());
+            Assert.That(((PostMessage)command).UserName, Is.EqualTo("Alice"));
+            Assert.That(((PostMessage)command).Message, Is.EqualTo("hello!"));
+        }
+    }
+
+    public class InputParser
+    {
+        public ICommand Parse(string input)
+        {
+            var splittedInput = input.Split(new[] {"->"}, StringSplitOptions.None).Select(i => i.Trim()).ToArray();
+            return new PostMessage(userName: splittedInput[0], message: splittedInput[1]);
         }
     }
 }
