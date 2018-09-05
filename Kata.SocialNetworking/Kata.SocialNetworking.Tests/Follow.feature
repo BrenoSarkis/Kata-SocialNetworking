238f1a3369e72c1ef67b6fe8a4dd53ee62bcd491
Breno Sarkis <sarkisbreno@gmail.com>
2018-09-05 18:07:52 +0200

    Adds scenario to cover the action of following another user

---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Tests/Follow.feature
@@ -0,0 +1,9 @@
+﻿Feature: Follow
+	In order to see other users posts
+	As a user
+	I want to subscribe to their posts
+
+Scenario: Follow an User
+	Given User posts message: "Bob -> hello world!"
+	When "Alice" follows "Bob"
+	Then "Alice"'s wall should contain: "Bob - hello world! (0 seconds ago)"
---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Tests/FollowSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Kata.SocialNetworking.Tests
+{
+    [Binding]
+    public class FollowSteps 
+    {
+        [Given(@"User posts message: ""(.*)""")]
+        public void GivenUserPostsMessage(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"""(.*)"" follows ""(.*)""")]
+        public void WhenFollows(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"""(.*)""'s wall should contain: ""(.*)""")]
+        public void ThenSWallShouldContain(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
