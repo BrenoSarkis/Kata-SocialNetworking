7668c4a98dcb2d56f080c28431670b3802fd6dc0
Breno Sarkis <sarkisbreno@gmail.com>
2018-09-14 23:23:03 +0200

    Clean up

--- Kata.SocialNetworking/Kata.SocialNetworking.Client/UserViewModel.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Client/UserViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kata.SocialNetworking.Client
+﻿namespace Kata.SocialNetworking.Client
 {
     public class UserViewModel
     {
