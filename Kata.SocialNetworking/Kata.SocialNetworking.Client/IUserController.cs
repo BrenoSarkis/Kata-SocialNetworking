2abe5add5fde325014a8eea06cbf17adfa17cb6f
Breno Sarkis <sarkisbreno@gmail.com>
2018-09-28 19:26:49 +0200

    Moves controller interface to its own file

---
+++ Kata.SocialNetworking/Kata.SocialNetworking.Client/IUserController.cs
@@ -0,0 +1,11 @@
+﻿using Kata.SocialNetworking.Messages.Follow;
+using Kata.SocialNetworking.Messages.Post;
+
+namespace Kata.SocialNetworking.Client
+{
+    public interface IUserController
+    {
+        void PostMessage(PostMessage postMessage);
+        void FollowUser(FollowUser followUser);
+    }
+}
--- Kata.SocialNetworking/Kata.SocialNetworking.Client/UserController.cs
+++ Kata.SocialNetworking/Kata.SocialNetworking.Client/UserController.cs
@@ -23,10 +23,4 @@ namespace Kata.SocialNetworking.Client
             bus.SendCommand(followUser);
         }
     }
-
-    public interface IUserController
-    {
-        void PostMessage(PostMessage postMessage);
-        void FollowUser(FollowUser followUser);
-    }
 }
